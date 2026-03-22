b100aa7e31bee5a91b3d3a9396b45eb36fa3e03d
Paul Haakma <paul_haakma@eagle.co.nz>
2026-03-22 13:42:25 +1300

    Bug fix for YouTube component styling

--- components/YouTube.tsx
+++ components/YouTube.tsx
@@ -7,7 +7,7 @@ const YouTube = ({ id, title = 'YouTube video' }: YouTubeProps) => {
   return (
     <div className="relative my-6 w-full overflow-hidden rounded-lg pb-[56.25%]">
       <iframe
-        className="absolute left-0 top-0 h-full w-full"
+        className="absolute top-0 left-0 h-full w-full"
         src={`https://www.youtube-nocookie.com/embed/${id}`}
         title={title}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
